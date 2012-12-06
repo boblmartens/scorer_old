04b3c5e6897807cce3cfaf1926cb89fafafb52ef
Bob Martens <bob@bobmartens.net>
2012-12-06 00:15:02 -0600

    Added ScorerGame class
    Will be the overall Game object for the application.

---
+++ Scorer/ScorerGame.h
@@ -0,0 +1,16 @@
+//
+//  ScorerGame.h
+//  Scorer
+//
+//  Created by Bob Martens on 12/6/12.
+//  Copyright (c) 2012 Deck78. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScorerGame : NSObject
+{
+    NSMutableDictionary *hands;
+}
+
+@end
---
+++ Scorer/ScorerGame.m
@@ -0,0 +1,13 @@
+//
+//  ScorerGame.m
+//  Scorer
+//
+//  Created by Bob Martens on 12/6/12.
+//  Copyright (c) 2012 Deck78. All rights reserved.
+//
+
+#import "ScorerGame.h"
+
+@implementation ScorerGame
+
+@end
