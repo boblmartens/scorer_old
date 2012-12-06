acb31d99b3e696fedc4de2c01a2c56f323c31b1b
Bob Martens <bob@bobmartens.net>
2012-12-05 23:47:08 -0600

    Initial Commit

---
+++ Scorer/ScorerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ScorerAppDelegate.h
+//  Scorer
+//
+//  Created by Bob Martens on 12/5/12.
+//  Copyright (c) 2012 Deck78. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScorerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Scorer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Scorer
+//
+//  Created by Bob Martens on 12/5/12.
+//  Copyright (c) 2012 Deck78. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ScorerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ScorerAppDelegate class]));
+    }
+}
