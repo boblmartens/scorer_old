b61903ee765e47a5cbc3fa978579e0eacb40b082
Bob Martens <bob@bobmartens.net>
2013-01-19 23:41:09 -0600

    Added dictionary of score data, is still working

--- Scorer/ScorerHand.m
+++ Scorer/ScorerHand.m
@@ -34,6 +34,30 @@
 {
     NSDictionary *bidScores = [NSDictionary dictionaryWithObjectsAndKeys:
                                [NSNumber numberWithInt:40], @"6Spades",
+                               [NSNumber numberWithInt:60], @"6Clubs",
+                               [NSNumber numberWithInt:80], @"6Diamonds",
+                               [NSNumber numberWithInt:100], @"6Hearts",
+                               [NSNumber numberWithInt:120], @"6NoTrump",
+                               [NSNumber numberWithInt:140], @"7Spades",
+                               [NSNumber numberWithInt:160], @"7Clubs",
+                               [NSNumber numberWithInt:180], @"7Diamonds",
+                               [NSNumber numberWithInt:200], @"7Hearts",
+                               [NSNumber numberWithInt:220], @"7NoTrump",
+                               [NSNumber numberWithInt:240], @"8Spades",
+                               [NSNumber numberWithInt:260], @"8Clubs",
+                               [NSNumber numberWithInt:280], @"8Diamonds",
+                               [NSNumber numberWithInt:300], @"8Hearts",
+                               [NSNumber numberWithInt:320], @"8NoTrump",
+                               [NSNumber numberWithInt:340], @"9Spades",
+                               [NSNumber numberWithInt:360], @"9Clubs",
+                               [NSNumber numberWithInt:380], @"9Diamonds",
+                               [NSNumber numberWithInt:400], @"9Hearts",
+                               [NSNumber numberWithInt:420], @"9NoTrump",
+                               [NSNumber numberWithInt:440], @"10Spades",
+                               [NSNumber numberWithInt:460], @"10Clubs",
+                               [NSNumber numberWithInt:480], @"10Diamonds",
+                               [NSNumber numberWithInt:500], @"10Hearts",
+                               [NSNumber numberWithInt:520], @"10NoTrump",
                                nil];
     
     int scoreSign;
